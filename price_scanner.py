47e96cb49bfda09d0fcd91430d15fa547579ba3d
bengarlock <ben.garlock@gmail.com>
2021-07-12 19:09:35 -0400

    sync commit

--- price_scanner.py
+++ price_scanner.py
@@ -3,14 +3,15 @@ import requests
 import time
 
 
+endpoint = 'https://bengarlock.com/api/v1/price_scanner/favorites/'
 
 
+favorites = requests.get(f'{endpoint}').json()
 
-urls = requests.get('https://bengarlock.com/api/v1/price_scanner/urls/').json()
-for url in urls:
-    print(url)
+for favorite in favorites:
+    print(favorite)
     driver = webdriver.Chrome('chromedriver.exe')
-    driver.get(url['url'])
+    driver.get(favorite['url'])
     item_name = driver.find_element_by_id("productTitle")
     try:
         price = driver.find_element_by_id("priceblock_ourprice")
@@ -18,5 +19,19 @@ for url in urls:
         price = driver.find_element_by_id("priceblock_saleprice")
 
     picture = driver.find_element_by_css_selector('#landingImage').get_attribute('src')
+
+    if favorite['name'] == '':
+        headers = {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+        }
+
+        payload = {
+            'name': item_name.text,
+            'image': picture
+        }
+
+        requests.patch(url=f'{endpoint}' + str(favorite['id']) + '/', data=payload)
+
     print(f"{item_name.text} - {price.text}")
     driver.quit()
