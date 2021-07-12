880c919586022171c0afdc5740ab0c9b89c654d4
bengarlock <ben.garlock@gmail.com>
2021-07-12 19:48:19 -0400

    price creation working

--- price_scanner.py
+++ price_scanner.py
@@ -5,50 +5,49 @@ import os
 
 endpoint = 'https://bengarlock.com/api/v1/price_scanner/favorites/'
 
-favorites = requests.get(f'{endpoint}').json()
-
-for favorite in favorites:
-    print(favorite)
-    driver = webdriver.Chrome('chromedriver.exe')
-    driver.get(favorite['url'])
-
 
 def detect_system():
     if platform.system() == 'Darwin':
         path = os.path.abspath("chromedriver")
         selenium_driver = webdriver.Chrome(path)
+        return selenium_driver
     elif platform.system() == 'Windows':
         path = os.path.abspath("chromedriver.exe")
         selenium_driver = webdriver.Chrome(path)
-    return selenium_driver
+        return selenium_driver
 
 
-urls = requests.get('https://bengarlock.com/api/v1/price_scanner/urls/').json()
-for url in urls:
-    print(url)
-    driver = detect_system()
-    driver.get(url['url'])
-    item_name = driver.find_element_by_id("productTitle")
-    try:
-        price = driver.find_element_by_id("priceblock_ourprice")
+def push_price(favorite, price):
+    print(favorite, price)
+    payload = {
+        'price': price,
+        'favorite': favorite['id']
+    }
+    requests.post(url='https://bengarlock.com/api/v1/price_scanner/prices/', data=payload)
 
-    except:
-        price = driver.find_element_by_id("priceblock_saleprice")
 
-    picture = driver.find_element_by_css_selector('#landingImage').get_attribute('src')
+def run_scan():
+    favorites = requests.get(f'{endpoint}').json()
+    for favorite in favorites:
+        driver = detect_system()
+        driver.get(favorite['url'])
+        item_name = driver.find_element_by_id("productTitle")
+        try:
+            price = driver.find_element_by_id("priceblock_ourprice")
+        except:
+            price = driver.find_element_by_id("priceblock_saleprice")
 
-    if favorite['name'] == '':
-        headers = {
-            'content-type': 'application/json',
-            'accept': 'application/json'
-        }
+        picture = driver.find_element_by_css_selector('#landingImage').get_attribute('src')
+        if favorite['name'] == '':
+            payload = {
+                'name': item_name.text,
+                'image': picture
+            }
+            requests.patch(url=f'{endpoint}' + str(favorite['id']) + '/', data=payload)
 
-        payload = {
-            'name': item_name.text,
-            'image': picture
-        }
+        # print(f"{item_name.text} - {price.text}")
+        push_price(favorite, price.text)
+        driver.quit()
 
-        requests.patch(url=f'{endpoint}' + str(favorite['id']) + '/', data=payload)
 
-    print(f"{item_name.text} - {price.text}")
-    driver.quit()
+run_scan()
